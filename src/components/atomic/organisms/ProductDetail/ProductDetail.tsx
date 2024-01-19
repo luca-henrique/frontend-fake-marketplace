45fa563f129c2da2d2791f6a6d34a3deec443f25
João Paulo <joao.elesbao@g.globo.com>
2024-01-18 22:35:51 -0300

    feat(product-details): criado componente de detalhe do produto

--- src/components/atomic/organisms/ProductDetail/ProductDetail.tsx
+++ src/components/atomic/organisms/ProductDetail/ProductDetail.tsx
@@ -1,4 +1,6 @@
-import { ProductType } from "@/types/Product";
+import { ProductType } from '@/types/Product';
+import './style.css';
+import { useState } from 'react';
 
 interface ProductDetailProps {
   product: ProductType;
@@ -7,26 +9,67 @@ interface ProductDetailProps {
 
 const ProductDetail: React.FC<ProductDetailProps> = ({ product, onClose }) => {
   const { id, title, price, description, images, category } = product;
+  const [selectedImage, setSelectedImage] = useState(images[0]);
 
+  const handleThumbnailClick = (image: string) => {
+    setSelectedImage(image);
+  };
   return (
-    <div style={{background:'red'}} className="modal-overlay">
-      <div className="modal">
-        <button className="close-button" onClick={onClose}>
-          Fechar
-        </button>
-        <h2>{title}</h2>
-        <p>ID: {id}</p>
-        <p>Preço: {price}</p>
-        <p>Descrição: {description}</p>
-        <p>Categoria: {category.name}</p>
-        <p>Imagens:</p>
-        <ul>
+    <div className="container-detail">
+      <button className="rounded-full bg-gray-200 text-gray-700 border-1 border-gray-400 px-4 py-2 absolute top-4 right-4">
+        Back
+      </button>
+      <div className="flex gap-4 md:gap-32 items-center p-4 md:p-20 mx-auto text-white">
+        <ul className="flex flex-col gap-4 md:gap-7 h-[100%]">
           {images.map((image, index) => (
             <li key={index}>
-              <img src={image} alt={`Product Image ${index + 1}`} />
+              <img
+                onClick={() => handleThumbnailClick(image)}
+                className={`rounded-md w-[90px] h-[90px] cursor-pointer ${
+                  selectedImage === image
+                    ? 'border border-gray-500 opacity-50'
+                    : ''
+                } max-w-full`}
+                src={image}
+                alt={`Thumbnail ${index + 1}`}
+              />
             </li>
           ))}
         </ul>
+
+        <div className="w-[50%] mx-auto">
+          <img
+            src={selectedImage}
+            className="w-full h-auto rounded-md w-[330px] h-auto mx-auto md:mx-0"
+          />
+        </div>
+
+        <div className="flex flex-col gap-30 w-1/2 text-white">
+          <div>
+            <h2 className="text-[20px] font-bold mb-6 ">{title}</h2>
+
+            <button className="rounded-full bg-gray-200 text-gray-700 border-1 border-gray-400 px-4 py-1">
+              {category.name}
+            </button>
+          </div>
+
+          <p className="my-6">Descrição: {description}</p>
+          <div className="flex justify-between items-center text-white">
+            <div>
+              <p className="">Preço</p>
+              <p className="text-xl font-bold">
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                }).format(price)}
+              </p>
+            </div>
+
+            <button className="bg-purple-700 text-white px-3 py-1 rounded-md">
+              Adicionar ao carrinho
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
